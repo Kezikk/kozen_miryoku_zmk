59aa5dd5a215ed23a0408c00fbb98973d6399411
Kozen <98872169+Kezikk@users.noreply.github.com>
2024-09-11 22:16:22 -0700

    Update custom_config.h
    Update outer columns

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -43,10 +43,10 @@ U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp 
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp TAB           K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BSPC \
-U_MT(LCTRL, ESC)  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp SQT \
-&kp LSHFT         K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-                            K32  K33  K34       K35  K36  K37
+&kp TAB          K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp BSPC \
+&u_mt LCTRL ESC  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
+&kp LSHFT        K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &u_tog_GAME \
+                           K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
